5b56e385f5d22544fc77df8b55108c97d0adf55d
naotoman <37291085+naotoman@users.noreply.github.com>
2025-01-03 23:16:12 +0900

    back to gpt

--- lib/lambda/sfnChatGpt/src/index.ts
+++ lib/lambda/sfnChatGpt/src/index.ts
@@ -59,7 +59,7 @@ interface ChatGptResponse {
     violates_ebay_policies: boolean;
     is_scam: boolean;
     not_japanese_product: boolean;
-    explanation: string;
+    result_explanation: string;
   };
   shipping_weight_and_box_dimensions: {
     weight: number;
@@ -70,12 +70,12 @@ interface ChatGptResponse {
     };
   };
   reselling_information: {
-    listing_title: string;
-    item_condition: string;
-    item_specifics: {
+    listing_title_for_ebay_listing: string;
+    item_condition_description_for_ebay_listing: string;
+    item_specifics_for_ebay_listing: {
       [key: string]: string | string[] | number | number[] | null;
     };
-    promotional_text: string;
+    promotional_text_for_ebay_listing: string;
   };
 }
 
@@ -97,16 +97,16 @@ const checklistSchema = {
       description:
         "Whether the seller explicitly states that the item originates from outside Japan.",
     },
-    explanation: {
+    result_explanation: {
       type: "string",
-      description: "Explanation of the checklist items.",
+      description: "Explanation of the checklist completion results.",
     },
   },
   required: [
     "violates_ebay_policies",
     "is_scam",
     "not_japanese_product",
-    "explanation",
+    "result_explanation",
   ],
   additionalProperties: false,
 };
@@ -130,52 +130,131 @@ const shippingWeightAndBoxDimensionsSchema = {
   additionalProperties: false,
 };
 
-const resellingInformationSchema = (aspectsFormatted: FormattedAspects) => {
+const resellingInformationSchema = (aspects: Aspect[]) => {
+  const formattedAspects = ((aspects: Aspect[]) => {
+    const transformType = (data: AspectConstraint) => {
+      if (data.itemToAspectCardinality === "MULTI") {
+        return "array";
+      } else if (data.aspectDataType === "NUMBER") {
+        return "number";
+      } else {
+        return "string";
+      }
+    };
+
+    const createDescription = (aspect: Aspect) => {
+      let desc = "";
+      if (aspect.aspectConstraint.itemToAspectCardinality === "SINGLE") {
+        desc += `A value for '${aspect.localizedAspectName}'. `;
+      } else {
+        desc += `Values for '${aspect.localizedAspectName}'. `;
+      }
+      if (
+        aspect.aspectConstraint.aspectMode === "FREE_TEXT" &&
+        aspect.aspectValues
+      ) {
+        desc +=
+          "(ex. " +
+          [...aspect.aspectValues]
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 20)
+            .map((v: AspectValue) => v.localizedValue)
+            .join(", ") +
+          ")";
+      }
+      if (!aspect.aspectConstraint.aspectRequired) {
+        desc += " Return null if not applicable.";
+      }
+      return desc;
+    };
+
+    return aspects.reduce((acc: FormattedAspects, aspect: Aspect) => {
+      if (
+        aspect.localizedAspectName === "MPN" ||
+        aspect.localizedAspectName === "California Prop 65 Warning" ||
+        aspect.localizedAspectName === "Unit Type" ||
+        aspect.localizedAspectName === "Unit Quantity" ||
+        aspect.localizedAspectName === "Item Width" ||
+        aspect.localizedAspectName === "Item Height" ||
+        aspect.localizedAspectName === "Item Weight" ||
+        aspect.localizedAspectName === "Item Length" ||
+        aspect.localizedAspectName === "Country/Region of Manufacture"
+      ) {
+        return acc;
+      }
+      const baseType = transformType(aspect.aspectConstraint);
+      acc[aspect.localizedAspectName] = {
+        type: aspect.aspectConstraint.aspectRequired
+          ? baseType
+          : [baseType, "null"],
+        description: createDescription(aspect),
+        ...(aspect.aspectConstraint.itemToAspectCardinality === "MULTI"
+          ? {
+              items: {
+                type:
+                  aspect.aspectConstraint.aspectDataType === "NUMBER"
+                    ? "number"
+                    : "string",
+              },
+            }
+          : {}),
+        ...(aspect.aspectConstraint.aspectMode === "SELECTION_ONLY"
+          ? {
+              enum: aspect.aspectValues.map(
+                (value: AspectValue) => value.localizedValue
+              ),
+            }
+          : {}),
+      };
+      return acc;
+    }, {});
+  })(aspects);
+
   return {
     type: "object",
     properties: {
-      listing_title: {
+      listing_title_for_ebay_listing: {
         type: "string",
         description:
           "A concise and descriptive title optimized for eBay's search engine (within 80 characters, including spaces).",
       },
-      item_condition: {
+      item_condition_description_for_ebay_listing: {
         type: "string",
-        description: "Brief explatation of the item's current condition.",
+        description: `Brief description of the item's current condition suitable for the eBay listing's "Condition Description" field.`,
       },
-      item_specifics: {
+      item_specifics_for_ebay_listing: {
         type: "object",
-        description: "Item specifics.",
-        properties: aspectsFormatted,
-        required: Object.keys(aspectsFormatted),
+        description: '"Item specifics" details suitable for the eBay listing.',
+        properties: formattedAspects,
+        required: Object.keys(formattedAspects),
         additionalProperties: false,
       },
-      promotional_text: {
+      promotional_text_for_ebay_listing: {
         type: "string",
         description:
           "Promotional text for the item. It should be a short and concise description of the item's features and benefits.",
       },
     },
     required: [
-      "listing_title",
-      "item_condition",
-      "item_specifics",
-      "promotional_text",
+      "listing_title_for_ebay_listing",
+      "item_condition_description_for_ebay_listing",
+      "item_specifics_for_ebay_listing",
+      "promotional_text_for_ebay_listing",
     ],
     additionalProperties: false,
   };
 };
 
-const chatgpt = async (event: Event, formattedAspects: FormattedAspects) => {
+const chatgpt = async (event: Event, aspects: Aspect[]) => {
   const prompt = {
     model: "gpt-4o",
     messages: [
       {
         role: "developer",
-        content: `You assist users in reselling items from Mercari Japan to international customers on eBay. Assume all items are pre-owned unless otherwise specified. Based on the provided item information from Mercari, perform the following tasks:
-1. Determine if the item is suitable for reselling on eBay using the risk checklist.
+        content: `You are running a cross-border reselling business, purchasing items from Mercari Japan and selling them to international customers on eBay. The process involves listing items on eBay based on existing Mercari listings, and then purchasing and shipping the item after an eBay sale. Given the following information from a Mercari listing (images, title, and description), perform the following tasks to prepare the item for eBay listing:
+1. Perform the risk checklist to assess whether the item is suitable for reselling on eBay.
 2. Estimate the total shipping weight (in grams) and the dimensions of the packaging box (in centimeters). Be cautious and aim slightly higher to avoid underestimation.
-3. Generate the information for listing the item on eBay in English. Avoid using phrases like "like new" or similar terms to prevent confusion for buyers.`,
+3. Generate the information for listing the item on eBay in English. Assume the item is pre-owned and avoid using phrases like "like new" or similar terms to prevent confusion for buyers.`,
       },
       {
         role: "user",
@@ -189,12 +268,15 @@ const chatgpt = async (event: Event, formattedAspects: FormattedAspects) => {
           })),
           {
             type: "text",
-            text: `<title>
+            text: `[Title of the listing on Mercari]
 ${event.item.orgTitle}
-</title>
-<description>
+`,
+          },
+          {
+            type: "text",
+            text: `[Description of the listing on Mercari]
 ${event.item.orgDescription}
-</description>`,
+`,
           },
         ],
       },
@@ -210,7 +292,7 @@ ${event.item.orgDescription}
             risk_checklist: checklistSchema,
             shipping_weight_and_box_dimensions:
               shippingWeightAndBoxDimensionsSchema,
-            reselling_information: resellingInformationSchema(formattedAspects),
+            reselling_information: resellingInformationSchema(aspects),
           },
           required: [
             "risk_checklist",
@@ -270,100 +352,17 @@ const calcShippingFee = (
   return Math.min(fedexFee, emsFee);
 };
 
-export const transformAspects = (aspects: Aspect[]) => {
-  const transformType = (data: AspectConstraint) => {
-    if (data.itemToAspectCardinality === "MULTI") {
-      return "array";
-    } else if (data.aspectDataType === "NUMBER") {
-      return "number";
-    } else {
-      return "string";
-    }
-  };
-
-  const createDescription = (aspect: Aspect) => {
-    let desc = "";
-    if (aspect.aspectConstraint.itemToAspectCardinality === "SINGLE") {
-      desc += `A value for '${aspect.localizedAspectName}'. `;
-    } else {
-      desc += `Values for '${aspect.localizedAspectName}'. `;
-    }
-    if (
-      aspect.aspectConstraint.aspectMode === "FREE_TEXT" &&
-      aspect.aspectValues
-    ) {
-      desc +=
-        "(ex. " +
-        [...aspect.aspectValues]
-          .sort(() => Math.random() - 0.5)
-          .slice(0, 20)
-          .map((v: AspectValue) => v.localizedValue)
-          .join(", ") +
-        ")";
-    }
-    if (!aspect.aspectConstraint.aspectRequired) {
-      desc += " Return null if not applicable.";
-    }
-    return desc;
-  };
-
-  return aspects.reduce((acc: FormattedAspects, aspect: Aspect) => {
-    if (
-      aspect.localizedAspectName === "MPN" ||
-      aspect.localizedAspectName === "California Prop 65 Warning" ||
-      aspect.localizedAspectName === "Unit Type" ||
-      aspect.localizedAspectName === "Unit Quantity" ||
-      aspect.localizedAspectName === "Item Width" ||
-      aspect.localizedAspectName === "Item Height" ||
-      aspect.localizedAspectName === "Item Weight" ||
-      aspect.localizedAspectName === "Item Length"
-    ) {
-      return acc;
-    }
-    const baseType = transformType(aspect.aspectConstraint);
-    acc[aspect.localizedAspectName] = {
-      type: aspect.aspectConstraint.aspectRequired
-        ? baseType
-        : [baseType, "null"],
-      description: createDescription(aspect),
-      ...(aspect.aspectConstraint.itemToAspectCardinality === "MULTI"
-        ? {
-            items: {
-              type:
-                aspect.aspectConstraint.aspectDataType === "NUMBER"
-                  ? "number"
-                  : "string",
-            },
-          }
-        : {}),
-      ...(aspect.aspectConstraint.aspectMode === "SELECTION_ONLY"
-        ? {
-            enum:
-              aspect.localizedAspectName === "Country/Region of Manufacture"
-                ? ["Japan"]
-                : aspect.aspectValues.map(
-                    (value: AspectValue) => value.localizedValue
-                  ),
-          }
-        : {}),
-    };
-    return acc;
-  }, {});
-};
-
 export const handler = async (event: Event) => {
   log(event);
   const aspects = JSON.parse(fs.readFileSync("69528.json", "utf8")).aspects;
-  const formattedAspects = transformAspects(aspects);
-  log(formattedAspects);
   //   chatgptで処理
-  const gptResult = await chatgpt(event, formattedAspects);
+  const gptResult = await chatgpt(event, aspects);
   if (
     gptResult.risk_checklist.violates_ebay_policies ||
     gptResult.risk_checklist.is_scam ||
     gptResult.risk_checklist.not_japanese_product
   ) {
-    throw new Error(gptResult.risk_checklist.explanation);
+    throw new Error(gptResult.risk_checklist.result_explanation);
   }
   // 入力を整形
   const shippingYen = calcShippingFee(
@@ -385,8 +384,8 @@ export const handler = async (event: Event) => {
       gptResult.shipping_weight_and_box_dimensions.box_dimensions.width,
       gptResult.shipping_weight_and_box_dimensions.box_dimensions.height,
     ],
-    ebayTitle: gptResult.reselling_information.listing_title,
-    ebayDescription: `<div style="color: rgb(51, 51, 51); font-family: Arial;"><p>${gptResult.reselling_information.promotional_text}</p><h3 style="margin-top: 1.6em;">Condition</h3><p>${gptResult.reselling_information.item_condition}</p><h3 style="margin-top: 1.6em;">Shipping</h3><p>Tracking numbers are provided to all orders. The item will be carefully packed to ensure it arrives safely.</p><h3 style="margin-top: 1.6em;">Customs and import charges</h3><p>Import duties, taxes, and charges are not included in the item price or shipping cost. Buyers are responsible for these charges. These charges may be collected by the carrier when you receive the item.</p></div>`,
+    ebayTitle: gptResult.reselling_information.listing_title_for_ebay_listing,
+    ebayDescription: `<div style="color: rgb(51, 51, 51); font-family: Arial;"><p>${gptResult.reselling_information.promotional_text_for_ebay_listing}</p><h3 style="margin-top: 1.6em;">Condition</h3><p>${gptResult.reselling_information.item_condition_description_for_ebay_listing}</p><h3 style="margin-top: 1.6em;">Shipping</h3><p>Tracking numbers are provided to all orders. The item will be carefully packed to ensure it arrives safely.</p><h3 style="margin-top: 1.6em;">Customs and import charges</h3><p>Import duties, taxes, and charges are not included in the item price or shipping cost. Buyers are responsible for these charges. These charges may be collected by the carrier when you receive the item.</p></div>`,
     ebayCategorySrc: [
       "Collectibles",
       "Animation Art & Merchandise",
@@ -395,9 +394,13 @@ export const handler = async (event: Event) => {
     ],
     ebayStoreCategorySrc: ["Anime Merchandise"],
     ebayConditionSrc: "Used",
-    ebayConditionDescription: gptResult.reselling_information.item_condition,
-    ebayAspectParam: Object.fromEntries(
-      Object.entries(gptResult.reselling_information.item_specifics)
+    ebayConditionDescription:
+      gptResult.reselling_information
+        .item_condition_description_for_ebay_listing,
+    ebayAspectParam: Object.fromEntries([
+      ...Object.entries(
+        gptResult.reselling_information.item_specifics_for_ebay_listing
+      )
         .filter(([_, value]) => {
           if (value == null || value === "") return false;
           if (Array.isArray(value)) {
@@ -407,7 +410,8 @@ export const handler = async (event: Event) => {
           }
           return true;
         })
-        .map(([key, value]) => [key, Array.isArray(value) ? value : [value]])
-    ),
+        .map(([key, value]) => [key, Array.isArray(value) ? value : [value]]),
+      ["Country/Region of Manufacture", ["Japan"]],
+    ]),
   };
 };
